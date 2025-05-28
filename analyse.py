f39696e1d6ce0919fd31d183e6e0f235eea175e3
stefanedenharter <stefan.edenharter@gmail.com>
2025-05-28 15:32:59 +0200

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 st.title("🧪 Clinical Trials Explorer")
 
 # Text input for clinical condition search
-query = st.text_input("Enter a condition or keyword (e.g., BPH, prostate cancer):", "BPH")
+query = st.text_input("Enter a device name, company, ... (e.g., iTind, Tigertriever):", "")
 
 # Start search on button click
 if st.button("Search"):
