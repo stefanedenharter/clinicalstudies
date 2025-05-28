a76660b26a2dbbdd10c1d117939ab4c692b6da2b
stefanedenharter <stefan.edenharter@gmail.com>
2025-05-28 14:47:22 +0200

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -43,10 +43,54 @@ if st.button("Search"):
                     end_date = status_mod.get("completionDateStruct", {}).get("date", "")
                     last_verified = status_mod.get("lastUpdatePostDateStruct", {}).get("date", "")
                     study_type = design_mod.get("studyType", "")
-
                     # enrollment = design_mod.get("enrollmentModule", {}).get("enrollmentCount", "")  # Optional: enable later
                     link = f"https://clinicaltrials.gov/study/{nct_id}"
 
+                  # from identificationModule
+                    # org_study_id = id_mod.get("orgStudyId", "")
+                    # acronym = id_mod.get("acronym", "")
+                    # brief_summary = id_mod.get("briefSummary", "")
+                    
+                    # from statusModule
+                    # study_first_submitted = status_mod.get("studyFirstSubmitDateStruct", {}).get("date", "")
+                    # study_first_posted = status_mod.get("studyFirstPostDateStruct", {}).get("date", "")
+                    # last_update_submitted = status_mod.get("lastUpdateSubmitDateStruct", {}).get("date", "")
+                    # last_update_posted = status_mod.get("lastUpdatePostDateStruct", {}).get("date", "")
+                    # completion_date_type = status_mod.get("completionDateStruct", {}).get("type", "")
+                    # start_date_type = status_mod.get("startDateStruct", {}).get("type", "")
+                    
+                    # from sponsorCollaboratorsModule
+                    # collaborator_names = [c.get("name", "") for c in sponsor_mod.get("collaborators", [])]
+                    
+                    # from designModule
+                    # study_type = design_mod.get("studyType", "")
+                    # allocation = design_mod.get("allocation", "")
+                    # intervention_model = design_mod.get("interventionModel", "")
+                    # primary_purpose = design_mod.get("primaryPurpose", "")
+                    # masking = design_mod.get("masking", "")
+                    
+                    # from descriptionModule
+                    # detailed_description = sec.get("descriptionModule", {}).get("detailedDescription", "")
+                    
+                    # from eligibilityModule
+                    # eligibility_mod = sec.get("eligibilityModule", {})
+                    # gender = eligibility_mod.get("sex", "")
+                    # minimum_age = eligibility_mod.get("minimumAge", "")
+                    # maximum_age = eligibility_mod.get("maximumAge", "")
+                    # eligibility_criteria = eligibility_mod.get("eligibilityCriteria", "")
+                    
+                    # from contactsLocationsModule
+                    # locations = sec.get("contactsLocationsModule", {}).get("locations", [])
+                    
+                    # from conditionsModule
+                    # conditions = sec.get("conditionsModule", {}).get("conditions", [])
+                    
+                    # from interventionsModule
+                    # interventions = sec.get("interventionsModule", {}).get("interventions", [])
+                    
+                    # from armsInterventionsModule
+                    # arms = sec.get("armsInterventionsModule", {}).get("arms", [])
+                    
                     records.append((nct_id, title, sponsor, status, start_date, end_date, last_verified, study_type, link))
                 except Exception:
                     continue
